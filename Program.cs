5f5541ca5c027096e97148fb25e9aa7d9b709eac
David Browning <david.a.browning@gmail.com>
2025-02-27 11:58:09 +0100

    Align number outputs.

--- Program.cs
+++ Program.cs
@@ -8,19 +8,19 @@
             // Application. Starts the Bankomat application.
             // ================================================================
             #region application            
-            Bank bank = new Bank();
-            Bankomat bankomat = new Bankomat(bank);
-            int accountsToCreate = 10;
-            bank.AddAccounts(accountsToCreate);
-            bankomat.Go();
+            //Bank bank = new Bank();
+            //Bankomat bankomat = new Bankomat(bank);
+            //int accountsToCreate = 10;
+            //bank.AddAccounts(accountsToCreate);
+            //bankomat.Go();
             #endregion application
 
             // ============================ REGION ============================
             // Tests. Uncomment to run unit tests.
             // ================================================================
             #region tests
-            //TestRunner testRunner = new TestRunner();
-            //testRunner.Go();
+            TestRunner testRunner = new TestRunner();
+            testRunner.Go();
             #endregion tests
         }
     }
